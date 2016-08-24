9387db20cd3e668d8d7978be78892e8b3a5dac6b
Naresh Anadkat <naresh.anadkat@gmail.com>
2016-08-24 00:07:14 -0400

    Add files via upload

--- run.py
+++ run.py
@@ -1,15 +1,56 @@
-from flask import Flask, request, redirect
-import twilio.twiml
+from flask import Flask, request, redirect, session
+from twilio import twiml
+import requests
+import json
 
+# The session object makes use of a secret key.
+SECRET_KEY = 'a secret key'
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Try adding your own number to this list!
+callers = {
+    "+16474541614": "Naresh",
+    "+14158675310": "Boots",
+    "+14158675311": "Virgil",
+}
+
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+    """Respond with the number of text messages sent between two parties."""
 
-    resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    counter = session.get('counter', 0)
+
+    # increment the counter
+    counter += 1
+
+    # Save the new counter value in the session
+    session['counter'] = counter
+
+    from_number = request.values.get('From')
+    if from_number in callers:
+        name = callers[from_number]
+    else:
+        name = "Monkey"
+
+    ticker = request.values.get("Body")
+
+    resp = twiml.Response()
+
+    response = requests.get("http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=" + ticker)
+
+    raw_json = response.text.split("(")[3].strip(")")  # convert to json
+    parsed_json = json.loads(raw_json)
+
+    if "LastPrice" in parsed_json:
+        last_price = parsed_json["LastPrice"]
+        resp.sms("The last price of " + ticker + " is " + str(last_price))
+    else:
+        resp.sms("Hi there! Thanks for using Naresh's Stock bot. To start off, please enter a valid ticker symbol you would like to know the current price of.")
+
     return str(resp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
